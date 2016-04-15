231d1160e2f534af5e549c07c277a81906f97ab6
PawelGt <pawel357@gmail.com>
2016-04-15 14:48:04 +0200

    Refactored_v2
    Working?

--- Manipulator/src/source/Controller_Ref.java
+++ Manipulator/src/source/Controller_Ref.java
@@ -83,11 +83,72 @@ public class Controller_Ref {
 		return rev;
 	}
 	
-	public double[] moveArmTo(float x, float y, float z) {
-		return connection.moveArmTo(x, y, z);
+	public int[] rotateMotorsToDeg(float alpha, float beta, float delta) {		
+		float alphaMin = -175.f,
+		alphaMax = 80.f,
+		betaMin = -58.f,
+		betaMax = 60.f,
+		deltaMin = -50.f,
+		deltaMax = 245.f;
+		
+		if (alpha < alphaMin) {
+			alpha = alphaMin;
+		}
+		else if (alpha > alphaMax) {
+			alpha = alphaMax;
+		}
+		
+		if (beta < betaMin) {
+			beta = betaMin;
+		}
+		else if (beta > betaMax) {
+			beta = betaMax;
+		}
+		
+		if (delta < deltaMin) {
+			delta = deltaMin;
+		}
+		else if (delta > deltaMax) {
+			delta = deltaMax;
+		}
+		
+		int[] anglesToEngine = CorrectDegrees(alpha, beta, delta);
+		connection.rotateMotorsTo(anglesToEngine[0], anglesToEngine[1], anglesToEngine[2]);
+		
+		while (connection.isMoving()) {
+			continue; //poczekaj a� silniki si� zatrzymaj�
+		}
+		return connection.getMotorPositions();
+	}
+	
+	public int[] rotateMotorsByDeg(float alpha, float beta, float delta) {
+		//need check for dangerous angles!!!!
+		//put here!
+		int[] anglesToEngine = CorrectDegrees(alpha, beta, delta);
+		connection.rotateMotorsBy(anglesToEngine[0], anglesToEngine[1], anglesToEngine[2]);
+		while (connection.isMoving()) {
+			continue; //poczekaj a� silniki si� zatrzymaj�
+		}
+		return connection.getMotorPositions();
+	}
+	
+	public double[] getArmPosition() {
+		int[] motorPositions = connection.getMotorPositions();
+		float[] angles = ReverseCorrectDegrees(motorPositions);
+		return Kinematics.calculateArmPosition(angles[0], angles[1], angles[2]);
+	}
+	
+	public double[] moveArmTo(float x, float y, float z) {		
+		for(int j = 0; j < 1; j++) {		
+			float[] ang = Kinematics.calculatechangeMotorPoisitons(x, y, z);
+			//Computed angles with small error send to device
+			rotateMotorsToDeg(ang[0], ang[1], ang[2]);
+		}	
+		return getArmPosition();
 	}
 	
 	public float[] moveArmBy(float x, float y, float z) {
-		return connection.moveArmBy(x, y, z);
+		// TODO
+		return null;
 	}
 }
--- Manipulator/src/source/PcConnection.java
+++ Manipulator/src/source/PcConnection.java
@@ -1,12 +1,13 @@
 package source;
 
+import lejos.nxt.Motor;
+
 public class PcConnection implements Connection{
 	private int[] startingPosition;
-	private int startSpeed, startSpeedA, startSpeedB, startSpeedC;
+	private int startSpeedA, startSpeedB, startSpeedC;
 	
 	public PcConnection(int startSpeed) {
-		startingPosition = getMotorPositions();
-		this.startSpeed = startSpeed;
+		startingPosition = getMotorPositions();		
 		startSpeedA = Math.round(startSpeed * 3.5f);
 		startSpeedB = Math.round(startSpeed * 0.45f);
 		startSpeedC = Math.round(startSpeed * 0.1f);
@@ -56,46 +57,6 @@ public class PcConnection implements Connection{
 		return getMotorPositions();
 	}
 	
-	public int[] rotateMotorsToDeg(float alpha, float beta, float delta) {		
-		float alphaMin = -175.f,
-		alphaMax = 80.f,
-		betaMin = -58.f,
-		betaMax = 60.f,
-		deltaMin = -50.f,
-		deltaMax = 245.f;
-		
-		if (alpha < alphaMin) {
-			alpha = alphaMin;
-		}
-		else if (alpha > alphaMax) {
-			alpha = alphaMax;
-		}
-		
-		if (beta < betaMin) {
-			beta = betaMin;
-		}
-		else if (beta > betaMax) {
-			beta = betaMax;
-		}
-		
-		if (delta < deltaMin) {
-			delta = deltaMin;
-		}
-		else if (delta > deltaMax) {
-			delta = deltaMax;
-		}
-		
-		int[] anglesToEngine = CorrectDegrees(alpha, beta, delta);
-		Motor.A.rotateTo(anglesToEngine[0], true);
-		Motor.B.rotateTo(anglesToEngine[1], true);
-		Motor.C.rotateTo(anglesToEngine[2], true);
-		
-		while (isMoving()) {
-			continue; //poczekaj a� silniki si� zatrzymaj�
-		}
-		return getMotorPositions();
-	}
-	
 	public int[] rotateMotorsBy(int alfa, int beta, int delta) {
 		Motor.A.rotate(alfa, true);
 		Motor.B.rotate(beta, true);
@@ -104,39 +65,6 @@ public class PcConnection implements Connection{
 			continue; //poczekaj a� silniki si� zatrzymaj�
 		}
 		return getMotorPositions();
-	}
-	
-	public int[] rotateMotorsByDeg(float alpha, float beta, float delta) {
-		//need check for dangerous angles!!!!
-		//put here!
-		int[] anglesToEngine = CorrectDegrees(alpha, beta, delta);
-		Motor.A.rotate(anglesToEngine[0], true);
-		Motor.B.rotate(anglesToEngine[1], true);
-		Motor.C.rotate(anglesToEngine[2], true);
-		while (isMoving()) {
-			continue; //poczekaj a� silniki si� zatrzymaj�
-		}
-		return getMotorPositions();
-	}
-	
-	public double[] getArmPosition() {
-		int[] motorPositions = getMotorPositions();
-		float[] angles = ReverseCorrectDegrees(motorPositions);
-		return Kinematics.calculateArmPosition(angles[0], angles[1], angles[2]);
-	}
-	
-	public double[] moveArmTo(float x, float y, float z) {		
-		for(int j = 0; j < 1; j++) {		
-			float[] ang = Kinematics.calculatechangeMotorPoisitons(x, y, z);
-			//Computed angles with small error send to device
-			rotateMotorsToDeg(ang[0], ang[1], ang[2]);
-		}	
-		return getArmPosition();
-	}
-	
-	public float[] moveArmBy(float x, float y, float z) {
-		// TODO
-		return null;
 	}
 	
 	public int[] reset() {
