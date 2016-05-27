f68f6f03c351f0fb6ed542bc5beb1a877f56e7f5
Stach <Stach@Lapcio>
2016-05-27 16:03:38 +0200

    Adapter

--- Manipulator/src/source/adapter/WhiteBoardAdapter.java
+++ Manipulator/src/source/adapter/WhiteBoardAdapter.java
@@ -5,16 +5,26 @@ import source.driver.Controller;
 public class WhiteBoardAdapter implements ControlAdapter2dTo3d {
 	private Controller controller;
 	private double currentAltitude;
-	private final double altOn = 20.0, altOff = 15.0;
+	private final double X0=12, Y0=30, X_MAX=0, Y_MAX=0;
+	private double currentX, currentY;
+	private final double altOn = 20.0, altOff = 0.0;
 	
 	public WhiteBoardAdapter(Controller controller) {
 		this.controller = controller;
+		currentX = X0;
+		currentY = Y0;
 		currentAltitude = altOn;
+	}
+	
+	private double[] execute(){
+		return controller.moveArmTo(currentX, currentAltitude, currentY);
 	}
 	
 	@Override
 	public double[] moveTo(double x, double y) {
 		// TODO Auto-generated method stub
+		currentX = x;
+		currentY = y;
 		return controller.moveArmTo(x, y, currentAltitude);
 	}
 	
